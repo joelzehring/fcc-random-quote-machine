43e618ef1c9cea10ec02dce85b1ed1a053852e55
joelzehring <joelzehring@gmail.com>
2019-10-05 22:42:07 -0700

    First commit, starting with multipass

---
+++ README.md
@@ -0,0 +1,15 @@
+#fcc Random Quote Machine
+
+*by Joel Zehring*
+
+## About
+
+A project to create a website that displays a random quote.
+
+## Index
+
+---
+
+## End
+
+[back to the top](#title)
---
+++ resources/javascript/app.js
@@ -0,0 +1,15 @@
+/* by Matthew Carpenter */
+console.log(`--------------------------------------------------------------------------------`);
+console.log(`Come check out my LinkedIn: https://www.linkedin.com/in/matthewcarpenter22`);
+console.log(`See my current projects on GitHub: https://www.github.com/getHashSet`);
+console.log(`--------------------------------------------------------------------------------`);
+console.log(`launching ./resources/javascript/app.js...`);
+$( document ).ready(function() {
+    console.log(`app.js ready!`);
+    /* ----- Code Start ----- */
+
+
+
+    /* ------ Code End ------ */
+});
+console.log(`loading...`);
