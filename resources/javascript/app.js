4f904a7e1dc6bfdd9829107ef0fe891aae3c10e2
joelzehring <joelzehring@gmail.com>
2019-10-12 22:54:38 -0700

    Trying to commit to a repo through command line.

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -7,7 +7,36 @@ console.log(`launching ./resources/javascript/app.js...`);
 $( document ).ready(function() {
     console.log(`app.js ready!`);
     /* ----- Code Start ----- */
+  
 
+const quoteData = [
+	{
+		quote: "An apple a day",
+		author: "Ben Frank"
+	},
+	{
+		quote: "I cannot tell a lie",
+		author: "George Wash"
+	},
+	{
+		quote: "Do or do not, or, whatever",
+		author: "Yo-dah"
+	},
+	{
+		quote: "Yeah, okay, um.. what?",
+		author: "Weird Itchy Cat"
+	}
+]
+
+const randNum = Math.floor(Math.random() * quoteData.length);
+$("#text").html("\"" + quoteData[randNum].quote + "\"");
+$("#author").html(quoteData[randNum].author); 
+
+$( "button" ).click(function( event ) {
+	const randNum = Math.floor(Math.random() * quoteData.length);
+	$("#text").html("\"" + quoteData[randNum].quote + "\"");
+	$("#author").html(quoteData[randNum].author); 
+});
 
 
     /* ------ Code End ------ */
